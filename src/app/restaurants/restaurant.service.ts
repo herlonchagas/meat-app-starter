84b5a7afee36dc4ec00be9e712b3169925ccf36e
herlonadaltech <herlon@adaltech.com.br>
2018-12-12 18:13:58 -0300

    teste2

---
+++ src/app/restaurants/restaurant.service.ts
@@ -0,0 +1,19 @@
+import { Restaurant } from './restaurant/restaurant.model';
+import { MEAT_API } from '../app.api';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class RestaurantService {
+  
+
+    constructor(private http: Http){}
+
+    restaurants(): Observable<Restaurant[]>{
+        return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+    }
+} 
