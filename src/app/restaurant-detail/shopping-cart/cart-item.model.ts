d5af926d8b6582c416d35ff1f6b208b510350b0c
herlonadaltech <herlon@adaltech.com.br>
2018-12-26 16:19:30 -0300

    Implementação do carrinho

---
+++ src/app/restaurant-detail/shopping-cart/cart-item.model.ts
@@ -0,0 +1,13 @@
+import { MenuItem } from '../menu-item/menuItem.model';
+
+
+export class CartItem {
+    constructor(public menuItem: MenuItem,
+                public quantity: number = 1){
+
+    }
+
+    value(): number {
+        return this.menuItem.price * this.quantity
+    }
+}
